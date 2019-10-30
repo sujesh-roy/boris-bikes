367607f914b4f284b6ede5a4139b08da0290fa41
Sonia Quintiao <soniaquintiao@gmail.com>
2019-10-30 14:52:05 +0000

    creates method and adds unit test for release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 require 'docking_station.rb'
 
 describe 'DockingStation' do
+  it 'can release bike' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:release_bike)
+  end
 
 end
